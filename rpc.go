bf550fbc1b4e329487bc287e9809b5babeb85c21
arturo-source <arturojuan9898@gmail.com>
2024-03-28 14:15:20 +0100

    Get full transaction info when getting block

--- rpc.go
+++ rpc.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 const RPC_URL = "https://cloudflare-eth.com"
@@ -16,33 +18,33 @@ type RPCRequest struct {
 	ID      int    `json:"id"`
 }
 
-type RPCResponse struct {
+type Block struct {
 	Jsonrpc string `json:"jsonrpc"`
 	Result  struct {
-		BaseFeePerGas         string   `json:"baseFeePerGas"`
-		BlobGasUsed           string   `json:"blobGasUsed"`
-		Difficulty            string   `json:"difficulty"`
-		ExcessBlobGas         string   `json:"excessBlobGas"`
-		ExtraData             string   `json:"extraData"`
-		GasLimit              string   `json:"gasLimit"`
-		GasUsed               string   `json:"gasUsed"`
-		Hash                  string   `json:"hash"`
-		LogsBloom             string   `json:"logsBloom"`
-		Miner                 string   `json:"miner"`
-		MixHash               string   `json:"mixHash"`
-		Nonce                 string   `json:"nonce"`
-		Number                string   `json:"number"`
-		ParentBeaconBlockRoot string   `json:"parentBeaconBlockRoot"`
-		ParentHash            string   `json:"parentHash"`
-		ReceiptsRoot          string   `json:"receiptsRoot"`
-		Sha3Uncles            string   `json:"sha3Uncles"`
-		Size                  string   `json:"size"`
-		StateRoot             string   `json:"stateRoot"`
-		Timestamp             string   `json:"timestamp"`
-		TotalDifficulty       string   `json:"totalDifficulty"`
-		Transactions          []string `json:"transactions"`
-		TransactionsRoot      string   `json:"transactionsRoot"`
-		Uncles                []any    `json:"uncles"`
+		BaseFeePerGas         string        `json:"baseFeePerGas"`
+		BlobGasUsed           string        `json:"blobGasUsed"`
+		Difficulty            string        `json:"difficulty"`
+		ExcessBlobGas         string        `json:"excessBlobGas"`
+		ExtraData             string        `json:"extraData"`
+		GasLimit              string        `json:"gasLimit"`
+		GasUsed               string        `json:"gasUsed"`
+		Hash                  string        `json:"hash"`
+		LogsBloom             string        `json:"logsBloom"`
+		Miner                 string        `json:"miner"`
+		MixHash               string        `json:"mixHash"`
+		Nonce                 string        `json:"nonce"`
+		Number                string        `json:"number"`
+		ParentBeaconBlockRoot string        `json:"parentBeaconBlockRoot"`
+		ParentHash            string        `json:"parentHash"`
+		ReceiptsRoot          string        `json:"receiptsRoot"`
+		Sha3Uncles            string        `json:"sha3Uncles"`
+		Size                  string        `json:"size"`
+		StateRoot             string        `json:"stateRoot"`
+		Timestamp             string        `json:"timestamp"`
+		TotalDifficulty       string        `json:"totalDifficulty"`
+		Transactions          []Transaction `json:"transactions"`
+		TransactionsRoot      string        `json:"transactionsRoot"`
+		Uncles                []any         `json:"uncles"`
 		Withdrawals           []struct {
 			Index          string `json:"index"`
 			ValidatorIndex string `json:"validatorIndex"`
@@ -54,31 +56,79 @@ type RPCResponse struct {
 	ID int `json:"id"`
 }
 
-func doRequest(data RPCRequest) (RPCResponse, error) {
-	var respData RPCResponse
+func doRequest(data RPCRequest) (Block, error) {
+	var block Block
 
 	buf := new(bytes.Buffer)
 	err := json.NewEncoder(buf).Encode(data)
 	if err != nil {
-		return respData, fmt.Errorf("error encoding data: %s", err)
+		return block, fmt.Errorf("error encoding data: %s", err)
 	}
 
 	req, err := http.NewRequest("POST", RPC_URL, buf)
 	if err != nil {
-		return respData, fmt.Errorf("error creating request: %s", err)
+		return block, fmt.Errorf("error creating request: %s", err)
 	}
 
 	req.Header.Set("Content-type", "application/json")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return respData, fmt.Errorf("error doing request: %s", err)
+		return block, fmt.Errorf("error doing request: %s", err)
 	}
 	defer resp.Body.Close()
 
-	err = json.NewDecoder(resp.Body).Decode(&respData)
+	err = json.NewDecoder(resp.Body).Decode(&block)
 	if err != nil {
-		return respData, fmt.Errorf("error decoding response: %s", err)
+		return block, fmt.Errorf("error decoding response: %s", err)
 	}
-	return respData, nil
+	return block, nil
+}
+
+func hexToDec(hex string) (int, error) {
+	numStr := strings.TrimPrefix(hex, "0x")
+	num, err := strconv.ParseInt(numStr, 16, 64)
+	if err != nil {
+		return 0, fmt.Errorf("error parsing number: %s", err)
+	}
+
+	return int(num), nil
+}
+
+func getLatestBlock() (int, error) {
+	data := RPCRequest{
+		Jsonrpc: "2.0",
+		Method:  "eth_getBlockByNumber",
+		Params:  []any{"latest", true},
+		ID:      1,
+	}
+
+	block, err := doRequest(data)
+	if err != nil {
+		return 0, fmt.Errorf("error getting block: %s", err)
+	}
+
+	num, err := hexToDec(block.Result.Number)
+	return num, err
+}
+
+func decToHex(dec int) string {
+	return fmt.Sprintf("0x%x", dec)
+}
+
+func getBlockByNumber(number int) (Block, error) {
+	numberHex := decToHex(number)
+	data := RPCRequest{
+		Jsonrpc: "2.0",
+		Method:  "eth_getBlockByNumber",
+		Params:  []any{numberHex, true},
+		ID:      1,
+	}
+
+	block, err := doRequest(data)
+	if err != nil {
+		return block, fmt.Errorf("error getting block: %s", err)
+	}
+
+	return block, nil
 }
