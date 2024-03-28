66090e91553685c17d0c5008fcd76e1ff2fabf5d
arturo-source <arturojuan9898@gmail.com>
2024-03-28 14:32:02 +0100

    Abstract main to only have 1 exit

--- main.go
+++ main.go
@@ -8,22 +8,23 @@ import (
 	"strconv"
 )
 
-func main() {
+func run() error {
 	port := flag.String("port", "8080", "port to listen on")
 	flag.Parse()
 
 	blockchain, err := newBlockchain()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error creating blockchain: %s\n", err)
-		os.Exit(1)
+		return fmt.Errorf("error creating blockchain: %s", err)
 	}
 
-	http.HandleFunc("/getCurrentBlock", func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/getCurrentBlock", func(w http.ResponseWriter, r *http.Request) {
 		blockNum := blockchain.GetCurrentBlock()
 		w.Write([]byte(strconv.Itoa(blockNum)))
 	})
 
-	http.HandleFunc("/suscribe", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/suscribe", func(w http.ResponseWriter, r *http.Request) {
 		address := r.URL.Query().Get("address")
 		alreadySubscribed := blockchain.Subscribe(address)
 		if alreadySubscribed {
@@ -33,14 +34,22 @@ func main() {
 		}
 	})
 
-	http.HandleFunc("/getTransactions", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/getTransactions", func(w http.ResponseWriter, r *http.Request) {
 		address := r.URL.Query().Get("address")
 		transactions := blockchain.GetTransactions(address)
 		w.Write([]byte(fmt.Sprintf("%v", transactions)))
 	})
 
-	if err := http.ListenAndServe(":"+*port, nil); err != nil {
-		fmt.Fprintf(os.Stderr, "Error starting server: %s\n", err)
+	if err := http.ListenAndServe(":"+*port, mux); err != nil {
+		return fmt.Errorf("error starting server: %s", err)
+	}
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
 		os.Exit(1)
 	}
 }
