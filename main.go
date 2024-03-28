2db522a03203ade4420d6c9778af6c0cfe815448
arturo-source <arturojuan9898@gmail.com>
2024-03-28 13:25:40 +0100

    Add http server to interact with program

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+)
 
+func main() {
+	port := flag.String("port", "8080", "port to listen on")
+	flag.Parse()
+
+	blockchain := Blockchain{}
+
+	http.HandleFunc("/getCurrentBlock", func(w http.ResponseWriter, r *http.Request) {
+		blockNum := blockchain.GetCurrentBlock()
+		w.Write([]byte(strconv.Itoa(blockNum)))
+	})
+
+	if err := http.ListenAndServe(":"+*port, nil); err != nil {
+		fmt.Fprintf(os.Stderr, "Error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
